6477d94aef7f3697ca46ead455b98d545406cbec
Elijah Walker <elijah@Elijahs-iMac.lan>
2022-11-27 15:12:57 -0600

    Created Random Tweet functionality, beginning styling, separating components

--- back-end/server.py
+++ back-end/server.py
@@ -1,6 +1,6 @@
 import requests, os
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Api
 from dotenv import load_dotenv
 from flask_cors import CORS
 
@@ -15,13 +15,13 @@ bearer_token = os.getenv("BEARER_TOKEN")
 
 headers = {"Authorization": f"Bearer {bearer_token}", "Accept": "application/json"}
 params = {
-        "expansions": "attachments.media_keys,author_id",
-        "media.fields": "url",
-        "tweet.fields": "created_at,public_metrics,entities,text",
-        "user.fields": "username,name,profile_image_url",
-        "tweet_mode": "extended",
-        "count": "10"
-    }
+    "expansions": "attachments.media_keys,author_id",
+    "media.fields": "url",
+    "tweet.fields": "created_at,public_metrics,entities,text",
+    "user.fields": "username,name,profile_image_url",
+    "tweet_mode": "extended",
+    "count": "10",
+}
 
 
 @app.route("/SearchTweets", methods=["GET"])
@@ -30,29 +30,26 @@ def get_tweets():
     user_input = args["userInput"]
 
     if user_input.startswith("@"):
-        search = f'from:{user_input[1:]}'
+        search = f"from:{user_input[1:]}"
     else:
-        search = f'{user_input} -is:retweet -is:reply'
+        search = f"{user_input} -is:retweet -is:reply"
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q={search}"
-
     tweet_response = requests.get(url, headers=headers, params=params)
     tweet_data = tweet_response.json()
 
     return tweet_data
 
+
 @app.route("/RandomTweet", methods=["GET"])
 def get_random_tweet():
     args = request.args
-    random_user= args['random_user']
-
+    random_user = args["random_user"]
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=from:{random_user}"
-
     random_tweet_response = requests.get(url, headers=headers, params=params)
     random_tweet_data = random_tweet_response.json()
 
     return random_tweet_data
-   
 
 
 if __name__ == "__main__":
--- front-end/src/components/Random/Random.js
+++ front-end/src/components/Random/Random.js
@@ -1,44 +1,63 @@
 import "./Random.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 import axios from "axios";
 
 const Random = () => {
-    const [text, setText] = useState("");
-    
+    const [randomUserData, setRandomUserData] = useState([])
+
     const favorite_twitter_users = [
-        "kurzegast",
-        "Jordan Peterson",
-        "Tim Kennedy",
-        "Mikael Akerfeldt",
-        "Ben Eller",
+        "@kurz_gesagt",
+        "@jordanbpeterson",
+        "@timkennedymma",
+        "@nasa",
+        "@neiltyson",
     ];
 
     const handleClick = (e) => {
         e.preventDefault();
         const randomUser = Math.floor(Math.random() * favorite_twitter_users.length);
         const result = favorite_twitter_users[randomUser];
-        setText(result);
-        
         getRandomData(result)
     };
 
     const getRandomData = (randomUser) => {
         axios.get("/RandomTweet", { params: { random_user: randomUser }}).then((randomTweet) => {
-            console.log(randomTweet)
-        })
-
+            setRandomUserData(randomTweet.data.statuses)
+        }).catch(err => console.log(err.message))
     }
+
+    const renderExtendedEntities = (randomTweet) => {
+        if(randomTweet.hasOwnProperty('extended_entities')){
+            return (<img src={randomTweet.extended_entities.media[0].media_url} alt='media' />)
+        }
+        return <></>
+    }
+
+    const display_random_tweets = () => {
+            const tweetRandomizer = Math.floor(Math.random() * 10)
+            const randomTweetList = randomUserData.map((randomTweet, key) => {
+                return (
+                        <div className="tweet" key={key}>
+                        {randomTweet.user.screen_name}
+                        {randomTweet.user.name}
+                        <img src={randomTweet.user.profile_image_url_https} alt={randomTweet.profile_image_url_https} />
+                        {randomTweet.full_text}
+                        {randomTweet.favorite_count}
+                        {randomTweet.retweet_count} 
+                        {renderExtendedEntities(randomTweet)}                    
+                    </div>
+                )
+            })
+                
+                return randomTweetList[tweetRandomizer]
+            }
 
     return (
         <div className="random-container">
             <h1 className="random-header">Random Tweet Page</h1>
-            <p>{text}</p>
-            {/* {randomData.map((tweet) => {
-                return <p>{tweet.text}</p>;
-            })} */}
-
-            <Button onClick={handleClick}>Get A Random Tweet!!!</Button>
+            {display_random_tweets()}
+            <Button onClick={handleClick}>Get A Random Tweet!</Button>
         </div>
     );
 };
