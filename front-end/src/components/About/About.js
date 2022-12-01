64f48a790d714c76c072fd1b543149d61b43953f
Elijah Walker <elijah@Elijahs-iMac.lan>
2022-11-30 19:00:27 -0600

    finished MVP

--- front-end/src/components/About/About.js
+++ front-end/src/components/About/About.js
@@ -1,21 +1,56 @@
 import "./About.css";
+import tweet_search from "../Images/dark-tweet.jpg";
+import random_tweet from "../Images/random-tweets.jpg";
 
 const About = () => {
     return (
         <div className="about-container">
-            <div className="about-text">
-                <h1 className="about-header">
-                    Welcome to the twitter showcase app!
-                </h1>
-                <p className="about-body">
-                    This application can search the Twitter API by either a
-                    username or a text-string match. Under the showcase tab, you
-                    are able to search the last 10 tweets of a specified user or
-                    you can search the most recent 10 tweets containing the
-                    entered string. Additionally, on the Random Tweets page, by
-                    clicking the button, you are able to pull a random tweet
-                    from my 5 favorite twitter users:{" "}
-                </p>
+            <h1 className="about-header">
+                Welcome to the Twitter Search and Random Tweet Generator App!
+            </h1>
+            <div className="about-body">
+                <div className="search-body">
+                    <img
+                        src={tweet_search}
+                        alt="search-tweets"
+                        className="search-tweets"
+                    />
+                    <div className="search-text">
+                        <h2 className="about-search-header">
+                            Tweet Search Feature
+                        </h2>
+                        <p>
+                            The tweet search features allows you to search for
+                            tweets by username or by string text match. If you
+                            begin your search with an @, an HTTP GET request is
+                            made to the server to specifically query that user.
+                            If the @ is omitted, this application will simply
+                            search the last 10 tweets containing a match to
+                            whatever string you've entered.
+                        </p>
+                    </div>
+                </div>
+                <div className="random-body">
+                    <img
+                        src={random_tweet}
+                        alt="random-tweets"
+                        className="random-tweets"
+                    />
+                    <div className="random-text">
+                        <h2 className="about-random-header">
+                            Random Tweet Generator
+                        </h2>
+                        <p>
+                            The Random Tweet Generator allows you to click a
+                            button to return a tweet from a set of my five
+                            favorite twitter users. Click will return a random
+                            user and a random tweet from their last 10 tweets.
+                            The users incluse Neil Degrasse Tyson, Scientific
+                            American, The American Natural History Museum,
+                            IFLScience, and NASA.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
